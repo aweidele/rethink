3729767ec30634173c2b5826a0ba5e850f28b3e4
Aaron Weidele <aweidele@gmail.com>
2018-07-09 20:14:45 -0400

    nav

--- functions.php
+++ functions.php
@@ -1,9 +1,9 @@
 <?php
 // Enqueue Scripts and Styles
 function enqueue_styles() {
-	// wp_enqueue_style( 'google_fonts',
-  //     '//fonts.googleapis.com/css?family=Cormorant+Garamond:400,400i,700|Open+Sans:300'
-  // );
+	wp_enqueue_style( 'google_fonts',
+      '//fonts.googleapis.com/css?family=Source+Sans+Pro:400,400i,600,700'
+  );
 
   wp_enqueue_style( 'main_style',
       get_stylesheet_directory_uri() . '/css/main.css'
@@ -17,3 +17,9 @@ function enqueue_styles() {
 	// );
 }
 add_action( 'wp_enqueue_scripts', 'enqueue_styles' );
+
+add_action( 'init', 'register_my_menu' );
+function register_my_menu() {
+  register_nav_menu( 'primary-menu', __( 'Primary Menu' ) );
+  //register_nav_menu( 'footer-menu', __( 'Footer Menu' ) );
+}
