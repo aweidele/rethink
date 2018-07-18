4cfb76e9bd7917753a8be2bf73ba6f67449031e8
Aaron Weidele <aweidele@gmail.com>
2018-07-18 07:42:00 -0400

    Front page

--- front-page.php
+++ front-page.php
@@ -2,8 +2,13 @@
   get_header();
 ?>
 <main class="content_main">
-  <p>Hello.</p>
-  
+<?php if(have_posts()) : while(have_posts()) : the_post(); ?>
+  <div class="frontpage_content">
+    <div class="frontpage_inner">
+<?php the_content(); ?>
+    </div>
+  </div>
+<?php endwhile; endif; ?>
 </main>
 <?php
   get_footer();
