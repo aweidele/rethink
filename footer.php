8f191b98a19fc7711a6cb4de9d307450481eae6f
Aaron Weidele <aweidele@gmail.com>
2018-07-19 07:24:47 -0400

    setting up news site

--- footer.php
+++ footer.php
@@ -1,5 +1,10 @@
 <?php
-$background_image = get_field("background_image",$post->ID);
+if(is_home()) {
+  $postID = get_option( 'page_for_posts' );
+} else {
+  $postID = $post->ID;
+}
+$background_image = get_field("background_image",$postID);
 if($background_image) { ?>
   <div class="background_image" style="background-image: url(<?=$background_image["url"]?>)"></div>
 <?php } ?>
--- index.php
+++ index.php
@@ -1,6 +1,23 @@
 <?php
   get_header();
+  $page_for_posts = get_option( 'page_for_posts' );
 ?>
-
+<main class="content_main">
+  <div class="content_wrapper">
+    <div class="content_medium">
+        News.
+<?php if(have_posts()) : while(have_posts()) : the_post(); ?>
+        <article class="news_post">
+          <header>
+            <h1><?php the_title(); ?></h1>
+          </header>
+          <main>
+            <?php the_content(); ?>
+          </main>
+        </article>
+<?php endwhile; endif; ?>
+    </div>
+  </div>
+</main>
 <?php
   get_footer();
