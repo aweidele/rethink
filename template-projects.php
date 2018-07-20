d9c0fc87e5e8c63bc08eea7f7746cca396a75cfd
Aaron Weidele <aweidele@gmail.com>
2018-07-20 08:14:45 -0400

    projects listing

--- template-projects.php
+++ template-projects.php
@@ -1,11 +1,25 @@
 <?php
   /* Template Name: Projects */
   get_header();
+  $args = [
+    "post_type" => "projects"
+  ];
+  $projects_query = new WP_Query($args);
 ?>
 <main class="content_main">
   <div class="content_wrapper">
-    <div class="projects">
-<?php if(have_posts()) : while(have_posts()) : the_post(); ?>
+    <div class="project_listing">
+<?php
+    if($projects_query -> have_posts()) : while($projects_query -> have_posts()) : $projects_query -> the_post();
+      $featured_image = get_field("featured_image");
+?>
+      <article class="project">
+        <a href="<?=get_permalink()?>">
+          <figure><img src="<?=$featured_image["sizes"]["project-listing"]?>"></figure>
+          <h2><?php the_title(); ?></h2>
+        </a>
+      </article>
+
         <!-- <article class="news_post">
           <header>
             <h1><a href="<?=get_permalink()?>"><?php the_title(); ?></a></h1>
