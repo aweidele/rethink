8cef91a0bafa1d1a571f186eea358c5fee0acb5a
Aaron Weidele <aweidele@gmail.com>
2018-08-12 17:33:55 -0400

    set up scrolling and additional content on front page

--- front-page.php
+++ front-page.php
@@ -2,13 +2,37 @@
   get_header();
 ?>
 <main class="content_main">
-<?php if(have_posts()) : while(have_posts()) : the_post(); ?>
-  <div class="frontpage_content">
+<?php
+  if(have_posts()) : while(have_posts()) : the_post();
+    $content = get_field("content");
+?>
+  <div class="frontpage_wrapper">
     <div class="frontpage_inner">
-<?php the_content(); ?>
+      <div class="frontpage_content">
+  <?php the_content(); ?>
+      </div>
     </div>
   </div>
-<?php endwhile; endif; ?>
+  <?php if($content) { ?>
+  <div class="content_wrapper">
+    <div class="content_medium">
+      <section class="page_blocks">
+        <?php
+          foreach($content as $row) {
+        ?>
+
+          <div class="content_row <?=$row["acf_fc_layout"]?>">
+            <?php include("callouts/col_".$row["acf_fc_layout"].".php"); ?>
+          </div>
+
+        <?php } ?>
+      </section>
+    </div>
+  </div>
+<?php
+  }
+  endwhile; endif;
+?>
 </main>
 <?php
   get_footer();
