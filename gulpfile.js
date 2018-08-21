ede8c17ee226318151a375fbab388cafa9f6dce2
R2INTEGRATED\aweidele <aweidele@r2integrated.com>
2018-08-21 11:30:07 -0400

    update autoprefixer

--- gulpfile.js
+++ gulpfile.js
@@ -29,7 +29,7 @@ gulp.task('sass', function(){
     .src('src/sass/main.scss')
     .pipe(sourcemaps.init())
     .pipe(sass(sassOptions).on('error', sass.logError))
-    .pipe(sourcemaps.write())
+    //.pipe(sourcemaps.write())
     .pipe(autoprefixer(autoprefixerOptions))
     .pipe(pxtorem())
     .pipe(gulp.dest('css/'))
