02f8969cf450976e5cb719e4b0d5ec007d7932b5
AARON WEIDELE <aaronweidele@AARONs-MacBook.local>
2018-11-19 05:56:21 -0500

    trying to add button

--- functions.php
+++ functions.php
@@ -92,6 +92,33 @@ function custom_post_type() {
 }
 
 add_action( 'init', 'custom_post_type', 0 );
+
+function wpb_mce_buttons_2($buttons) {
+    array_unshift($buttons, 'styleselect');
+    return $buttons;
+}
+add_filter('mce_buttons_2', 'wpb_mce_buttons_2');
+
+// Callback function to filter the MCE settings
+function my_mce_before_init_insert_formats( $init_array ) {
+	// Define the style_formats array
+	$style_formats = array(
+		// Each array child is a format with it's own settings
+		array(
+			'title' => 'Intro Text',
+			'block' => 'p',
+			'classes' => 'intro-text',
+			'wrapper' => false,
+		)
+	);
+	// Insert the array, JSON ENCODED, into 'style_formats'
+	$init_array['style_formats'] = json_encode( $style_formats );
+
+	return $init_array;
+
+}
+// Attach callback to 'tiny_mce_before_init'
+add_filter( 'tiny_mce_before_init', 'my_mce_before_init_insert_formats' );
 /*
 http://omfgitsnater.com/2012/06/using-flush_rewrite_rules-with-your-custom-post-types/
 */
